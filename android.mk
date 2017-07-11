e43ed9b7b36dd074e315bfc9c5fe04c8af78e509
root <root@localhost>
2017-07-11 23:21:50 +0200

    Android Makefile

---
+++ android.mk
@@ -0,0 +1,18 @@
+OBJS := $(wildcard src/*.cc)
+CXX := g++
+RM := rm
+INCLUDE_PATHS := -Iinclude
+LIBRARY_PATHS := -DDEBUG
+
+CXXFLAGS := --std=c++14 -Wall -Wextra -Wstrict-aliasing -pedantic -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option 
+
+LDFLAGS := -lm -ldl -llog -lz -lSDL2_image -lSDL2_net -ltiff -ljpeg -lpng -lz -lSDL2_ttf -lfreetype -lSDL2_mixer -lSDL2_test -lsmpeg2 -lvorbisfile -lvorbis -logg -lstdc++ -lSDL2 -lEGL -lGLESv1_CM -lGLESv2 -landroid 
+OBJ_NAME := game
+
+.PHONY: all clean
+
+all : $(OBJS)
+	$(CXX) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(CXXFLAGS) $(LDFLAGS) -o $(OBJ_NAME)
+
+clean:
+	$(RM) $(OBJ_NAME)
