b4ca700be6fef0e4991c8301d7cc90ebf0c3f6be
root <root@localhost>
2017-07-13 12:31:07 +0200

    Fix Android Makefile

--- android.mk
+++ android.mk
@@ -2,12 +2,12 @@ OBJS := $(wildcard src/*.cc)
 CXX := g++
 RM := rm
 INCLUDE_PATHS := -Iinclude
-LIBRARY_PATHS := -DDEBUG
+LIBRARY_PATHS := -DDEBUG -DANDROID
+OBJ_NAME := game
 
 CXXFLAGS := --std=c++14 -Wall -Wextra -Wstrict-aliasing -pedantic -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option 
 
 LDFLAGS := -lm -ldl -llog -lz -lSDL2_image -lSDL2_net -ltiff -ljpeg -lpng -lz -lSDL2_ttf -lfreetype -lSDL2_mixer -lSDL2_test -lsmpeg2 -lvorbisfile -lvorbis -logg -lstdc++ -lSDL2 -lEGL -lGLESv1_CM -lGLESv2 -landroid 
-OBJ_NAME := game
 
 .PHONY: all clean
 
