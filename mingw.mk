3560d516d3eec5ae1bca1b15d025443ab446c97b
Ralph Möritz <ralphmoritz@outlook.com>
2017-07-15 12:23:38 +0200

    Update Makefiles

--- mingw.mk
+++ mingw.mk
@@ -3,11 +3,11 @@ CXX := g++
 RM := del
 INCLUDE_PATHS := -Iinclude
 LIBRARY_PATHS := -DDEBUG
+OBJ_NAME := game.exe
 
-CXXFLAGS := --std=c++1z -Wall -Wextra -Wstrict-aliasing -pedantic -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -Wl,-subsystem,windows
+CXXFLAGS := --std=c++14 -Wall -Wextra -Wstrict-aliasing -pedantic -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -Wl,-subsystem,windows
 
 LDFLAGS := -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
-OBJ_NAME := game.exe
 
 .PHONY: all clean
 
