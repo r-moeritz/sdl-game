74b32f68b27a3d87b061ddcd6b87d764d98cbb2d
Ralph Moeritz <ralphmoritz@outlook.com>
2018-12-27 00:03:05 +0100

    Page 174 (WIP)

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 OBJS := $(wildcard src/*.cc)
 CXX := g++
 RM := rm
-INCLUDE_PATHS := -Iinclude
+INCLUDE_PATHS := -Iinclude -Iext/cppcodec -Iext/zstr/src
 LIBRARY_PATHS := -DDEBUG
 OBJ_NAME := game
 
 CXXFLAGS := --std=c++14 -Wall -Wextra -Wstrict-aliasing -pedantic -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option
 
-LDFLAGS := -g -lSDL2main -lSDL2 -lSDL2_image -ltinyxml
+LDFLAGS := -g -lSDL2main -lSDL2 -lSDL2_image -ltinyxml -lz
 
 .PHONY: all clean
 
--- src/levelparser.cc
+++ src/levelparser.cc
@@ -1,6 +1,11 @@
 #include "levelparser.hh"
 #include "level.hh"
-#include "tinyxml.h"
+#include "texturemanager.hh"
+#include "game.hh"
+#include <tinyxml.h>
+#include <cppcodec/base64_rfc4648.hpp>
+#include <zstr.hpp>
+#include <stdexcept>
 
 using namespace MyGame;
 
@@ -35,11 +40,53 @@ LevelPtr LevelParser::parseLevel(const char* levelFile) {
 
 void LevelParser::parseTilesets(TiXmlElement* pTilesetRoot,
                                 TilesetVectorPtr pTilesets) {
+  // Add tileset to texture manager
+  TextureManager::instance()->load(pTilesetRoot->FirstChildElement()->Attribute("source"),
+                                   pTilesetRoot->Attribute("name"),
+                                   Game::instance()->renderer());
 
+  // Create a tileset object
+  Tileset ts;
+  pTilesetRoot->FirstChildElement()->Attribute("width", &ts.width);
+  pTilesetRoot->FirstChildElement()->Attribute("height", &ts.height);
+  pTilesetRoot->Attribute("firstgid", &ts.firstGridID);
+  pTilesetRoot->Attribute("tilewidth", &ts.tileWidth);
+  pTilesetRoot->Attribute("tileheight", &ts.tileHeight);
+  pTilesetRoot->Attribute("spacing", &ts.spacing);
+  pTilesetRoot->Attribute("margin", &ts.margin);
+  ts.name = pTilesetRoot->Attribute("name");
+  ts.numColumns = ts.width / (ts.tileWidth + ts.spacing);
+
+  pTilesets->push_back(ts);
 }
 
 void LevelParser::parseTileLayer(TiXmlElement* pTileElement,
                                  LayerPtrVectorPtr pLayers,
                                  TilesetVectorPtr pTilesets) {
+  using base64 = cppcodec::base64_rfc4648;
 
+  auto pTileLayer = std::make_shared<TileLayer>(_tileSize, *pTilesets);
+
+  // Tile data
+  std::vector<std::vector<int>> data;
+
+  TiXmlElement* pDataNode = nullptr;
+
+  for (auto pe = pTileElement->FirstChildElement();
+       pe != nullptr;
+       pe = pe->NextSiblingElement()) {
+    if (pe->Value() == std::string("data")) {
+      pDataNode = pe;
+      break;
+    }
+  }
+
+  if (!pDataNode) {
+    throw std::runtime_error("No data node");
+  }
+
+  std::string text = pDataNode->FirstChild()->ToText()->Value();
+  std::vector<unsigned> decodedIds = base64::decode(text);
+
+  // TODO Uncompress decodedIds
 }
